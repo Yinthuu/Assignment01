b7562566efa91206f39d750b3b01e93ec0c87b83
Fantasic Four <yin.thu.aye0@gmail.com>
2022-10-11 20:43:52 -0400

    Initial Commit

---
+++ Assignment01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment01
+//
+//  Created by Yin Thu Aye on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
